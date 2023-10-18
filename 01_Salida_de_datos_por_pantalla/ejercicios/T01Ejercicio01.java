ec560bb266a06c3439f6b5207d211c346a5aaf30
Sergio Sanchez Martin <ssanmar580q@g.educaand.es>
2023-10-18 14:42:45 +0200

    Se cambia nombre de clase en T01EJ01

--- 01_Salida_de_datos_por_pantalla/ejercicios/T01Ejercicio01.java
+++ 01_Salida_de_datos_por_pantalla/ejercicios/T01Ejercicio01.java
@@ -3,7 +3,7 @@
  *
  * @author Sergio Sánchez Martín
  */
-public class S01Ejercicio01 {
+public class T01Ejercicio01 {
   public static void main(String[] args) {
     System.out.println("Sergio Sánchez Martín");
   }
