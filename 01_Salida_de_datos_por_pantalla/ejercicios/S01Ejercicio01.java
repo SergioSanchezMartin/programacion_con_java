31856fb20e7cc24cf058a3bded465a0be520afbb
Sergio Sanchez Martin <ssanmar580q@g.educaand.es>
2023-10-18 14:08:34 +0200

    Añadida la solución del Ejercicio 1 del Tema 1

---
+++ 01_Salida_de_datos_por_pantalla/ejercicios/S01Ejercicio01.java
@@ -0,0 +1,10 @@
+/**
+ * 1. Escribe una programa que muestre tu nombre por pantalla.
+ *
+ * @author Sergio Sánchez Martín
+ */
+public class S01Ejercicio01 {
+  public static void main(String[] args) {
+    System.out.println("Sergio Sánchez Martín");
+  }
+}
