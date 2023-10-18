680e47ab6c6d8724702f879669fee951491cfb47
Sergio Sanchez Martin <ssanmar580q@g.educaand.es>
2023-10-18 13:44:56 +0200

    Añadida la clase CaracteresEspeciales01.java

---
+++ 01_Salida_de_datos_por_pantalla/ejemplos/CaracteresEspeciales01.java
@@ -0,0 +1,11 @@
+/**
+ * Muestra caracteres especiales UNICODE
+ * 
+ * @author Sergio Sánchez
+ */
+public class CaracteresEspeciales01 {
+  public static void main(String[] args) {
+    System.out.print("Cuando programo \u2328  estoy");
+    System.out.print(" muy \u263A y me siento como un \u2654 o como una \u2655 .");
+  }
+}
