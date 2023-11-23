49931b9c067b74cfe5e1e57e7fbaebae764b9b6f
Sergio Sanchez Martin <ssanmar580q@g.educaand.es>
2023-11-23 19:13:12 +0100

    Se añaden más ejercicios del Tema 5 mandados para el 23/11/2023

--- 05_Bucles/ejercicios/T05Ejercicio37.java
+++ 05_Bucles/ejercicios/T05Ejercicio37.java
@@ -26,14 +26,14 @@ public class T05Ejercicio37 {
     int digito = 0;
 
     // Separa las cifras y escribe los palitos
-    for (int i = 1; i <= numeroDigitos; i++){
+    for (int i = 0; i < numeroDigitos; i++){
       digito = numeroVolteado%10;
       numeroVolteado /= 10;
-      for (int j = 1; j <= digito; j++){
+      for (int j = 0; j < digito; j++){
         System.out.print("|");
       }
       //Mientras no sea la ultima cifra, pintamos el guion separador
-      if (i < numeroDigitos){
+      if (i < numeroDigitos - 1){
         System.out.print("-");
       }
     }
---
+++ 05_Bucles/ejercicios/T05Ejercicio39.java
@@ -0,0 +1,28 @@
+/**
+ * Tema 5 - Bucles
+ *
+ * 39. Escribe un programa que pida un número entero positivo por teclado y que
+ * muestre a continuación los números desde el 1 al número introducido junto
+ * con su factorial.
+ * 
+ * @author Sergio Sánchez
+ */
+import java.util.Scanner;
+
+public class T05Ejercicio39 {
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    System.out.print("Por favor, introduzca un número entero positivo: ");
+    int numeroIntroducido = sc.nextInt();
+    sc.close();
+
+    for (int n = 1; n <= numeroIntroducido; n++) {
+      int factorial = 1;    //Empezamos por un valor inicial al comienzo de cada iteración
+      
+      for (int i = 1; i <= n; i++) {
+        factorial *= i;
+      }
+      System.out.println(n + "! = " + factorial);
+    }
+  }
+}
