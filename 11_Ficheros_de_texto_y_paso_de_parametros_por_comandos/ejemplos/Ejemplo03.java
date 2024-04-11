ae81bb0c55d61459fdf5ff9a773926cc1b61ee7b
Sergio Sánchez Martín <144800117+SergioSanchezMartin@users.noreply.github.com>
2024-04-11 20:39:00 +0200

    Ejemplo del tema 11

---
+++ 11_Ficheros_de_texto_y_paso_de_parametros_por_comandos/ejemplos/Ejemplo03.java
@@ -0,0 +1,29 @@
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+/**
+ * Ejemplo de escritura en un fichero de texto
+ *
+ */
+
+public class Ejemplo03 {
+  public static void main(String[] args) {
+    try {
+      BufferedWriter bw = new BufferedWriter(new FileWriter("ficheros/fruta.txt"));
+
+      bw.write("aguacate");
+      bw.newLine();
+      bw.write("mango");
+      bw.newLine();
+      bw.write("níspero\n");
+
+      bw.close();
+
+    } catch (IOException ioe) {
+      System.out.println("No se ha podido escribir en el fichero. " + ioe.getMessage());
+    }
+    
+  }
+
+}
